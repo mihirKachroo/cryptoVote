7fc45887a63ce9021f008d33e499d91b0a8f3e32
Bill Bai <31229051+billbai0102@users.noreply.github.com>
2020-05-30 19:46:20 -0400

    vote display (kinda)

---
+++ WebApp - Display Winners/results/src/component.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+const request = require('request')
+
+export default class Data extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            biden: 'Please wait...',
+            trump: 'Please wait...'
+        }
+    }
+
+    betterSetState(biden, trump) {
+        this.setState({
+            biden: biden,
+            trump: trump
+        })
+    }
+
+    findWinner() {
+        let bidenVotes = 0
+        let trumpVotes = 0
+        fetch('http://localhost:1234/GET-Ballots')
+            .then(data => data.json())
+            .then((body) => {
+                body = body.data
+
+                for (let key in Object.keys(body)) {
+                    let temp = body[key].votedFor
+                    if (temp === "Justin Trudeau") {
+                        bidenVotes += 1
+                    } else if (temp === "Donald Trump") {
+                        trumpVotes += 1
+                    }
+                }
+                this.setState({
+                    biden: bidenVotes,
+                    trump: trumpVotes
+                })
+                console.log(`Biden Votes: ${bidenVotes} - Trump Votes: ${trumpVotes}`)
+            })
+    }
+
+    render() {
+        this.findWinner()
+        return (
+            <div className="App">
+                <div className={"Title"}>
+                    
+                </div>
+                <h2>Total Votes for Joseph Biden: {this.state.biden}</h2>
+                <h2>Total Votes for Donald Trump: {this.state.biden}</h2>
+            </div>
+        )
+    }
+}
