7f251c681badb6d403a6050ead4db63464ece33e
Bill Bai <31229051+billbai0102@users.noreply.github.com>
2020-05-30 23:21:40 -0400

    firebase

---
+++ webApp/build/precache-manifest.e2ca298ed125bf8e81642e3e0688935d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77f5523cd3f2af4cc6d300c011417e6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc8cb22d4a10d9a52f2f",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "7e1d886f558ec2b70827",
+    "url": "/static/css/main.d333b982.chunk.css"
+  },
+  {
+    "revision": "bc8cb22d4a10d9a52f2f",
+    "url": "/static/js/2.02bea18e.chunk.js"
+  },
+  {
+    "revision": "7e1d886f558ec2b70827",
+    "url": "/static/js/main.92e31c19.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f6a66893aa4ee58144a9eaf5a2816347",
+    "url": "/static/media/biden2.f6a66893.jpg"
+  },
+  {
+    "revision": "9a4462a6c1685f324935ee7b4c6da3fd",
+    "url": "/static/media/trump2.9a4462a6.jpg"
+  }
+]);
